8b5e21e9a6f7d30401ab32da650e086fef4ddd71
Shannon Lo <slo2695@gmail.com>
2021-07-12 11:22:26 -0700

    Added ability to search most recent tweets from specific account

--- nhl_exp_functions.py
+++ nhl_exp_functions.py
@@ -638,3 +638,53 @@ def print_lda_keywords_weight(data, number_of_topics):
         st.warning('**Theme #**' + str(topic_num) + '**:** ' + list_topic_words_weight)
         # increments topic number by 1 so that each theme printed out will have a new number
         topic_num += 1
+
+# Function 20
+#----------------
+# Function to create dataframe of most recent 3240 tweets from a specific user
+def get_user_tweets(screen_name):
+    #Twitter only allows access to a users most recent 3240 tweets with this method
+    #Adapted by: https://gist.github.com/yanofsky/5436496?fbclid=IwAR12gb56FOxTNI6R3SfiwpnbPpTvKLoeGR3kP0peQ1nGilcwsF8bR0LSVqE
+    
+    # Set up Twitter API access
+    # Define access keys and tokens
+    consumer_key = st.secrets['consumer_key']
+    consumer_secret = st.secrets['consumer_secret']
+    access_token = st.secrets['access_token']
+    access_token_secret = st.secrets['access_token_secret']
+
+    auth = tw.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tw.API(auth, wait_on_rate_limit = True)
+
+    #initialize a list to hold all the tweepy Tweets
+    alltweets = []  
+    
+    #make initial request for most recent tweets (200 is the maximum allowed count)
+    new_tweets = api.user_timeline(screen_name = screen_name,count=200)
+    
+    #save most recent tweets
+    alltweets.extend(new_tweets)
+    
+    #save the id of the oldest tweet less one
+    oldest = alltweets[-1].id - 1
+    
+    #keep grabbing tweets until there are no tweets left to grab
+    while len(new_tweets) > 0:
+        
+        #all subsiquent requests use the max_id param to prevent duplicates
+        new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
+        
+        #save most recent tweets
+        alltweets.extend(new_tweets)
+        
+        #update the id of the oldest tweet less one
+        oldest = alltweets[-1].id - 1
+    
+    #transform the tweepy tweets into a 2D array that will populate the csv 
+    outtweets = [[tweet.id_str, tweet.created_at, tweet.text, tweet.retweet_count, tweet.favorite_count] for tweet in alltweets]
+
+    #transform 2D array into pandas dataframe
+    df_tweets = pd.DataFrame(data=outtweets, columns=['id_str', 'created_at', 'text', 'rt_count', 'fav_count'])
+
+    return df_tweets
--- pages/insiders.py
+++ pages/insiders.py
@@ -1,19 +1,73 @@
+#--------------------------------------------------
+# PART 1: LOAD DEPENDENCIES & TODO
+#--------------------------------------------------
+# - 1.1: Load libraries
+# - 1.2: Load custom library
+# - 1.3: TODO items
+#--------------------------------------------------
+
+#~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=
+# 1.1: Load libraries
+#------------------------------------#
 import streamlit as st
 import numpy as np
 import pandas as pd
 from sklearn import datasets
 
+#~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=
+# 1.2: Load custom library
+#------------------------------------#
+import nhl_exp_functions as nf # custom functions file
+#~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=
+
+#~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=
+# 1.3: #TODO
+#------------------------------------#
+
+#~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~--=~-=~-=~-=~-=~-=~-
+# PART 2: APP UI SETUP
+#~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~--=~-=~-=~-=~-=~-=~-
+# - 2.1: Main panel setup 
+# - 2.2: Sidebar setup
+#~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~--=~-=~-=~-=~-=~-=~-
+
+# Everything must be defined within the app function
 def app():
+
+    ## 2.1.1: Main Panel Title
+    ##----------------------------------##
     st.title('NHL Insiders View')
 
-    st.write("This is the `Data` page of the multi-page app.")
+    #col1, col2 = st.beta_columns((1,1)) # 2 same sized columns
 
-    st.write("The following is the DataFrame of the `iris` dataset.")
+    ## 2.2.1: Sidebar Title
+    ##----------------------------------##
+    st.sidebar.header('Choose Search Inputs') #sidebar title
+    
 
-    iris = datasets.load_iris()
-    X = pd.DataFrame(iris.data, columns = iris.feature_names)
-    Y = pd.Series(iris.target, name = 'class')
-    df = pd.concat([X,Y], axis=1)
-    df['class'] = df['class'].map({0:"setosa", 1:"versicolor", 2:"virginica"})
+    ## 2.2.2: Sidebar Input Fields
+    ##----------------------------------##
+    with st.form(key ='form_1'):
+        with st.sidebar:
+            insider_account_name = st.text_input("1. Enter one Twitter account", help='Enter a Twitter account handle without the @')
+            st.sidebar.text("") # spacing
+            submitted1 = st.form_submit_button(label = 'Run Tweet Analyzer 🚀', help = 'Re-run analyzer with the current inputs')
 
-    st.write(df)
+
+
+#~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~--=~-=~-=~-=~-=~-=~-
+# PART 3: APP DATA SETUP
+#~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~--=~-=~-=~-=~-=~-=~-
+# - 3.1: Twitter data ETL
+# - 3.2: Define key variables
+#~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~-=~--=~-=~-=~-=~-=~-=~-
+
+    # 3.1: Twitter Data ETL
+
+    # Layout
+    #------------------------------------#
+
+    # Run function 2: Get twitter data 
+    df_user_tweets = nf.get_user_tweets(insider_account_name)
+
+    st.write(df_user_tweets)
